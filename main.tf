a49ab33b9cd16e1e424b92aeacba53724cb1debf
Fikiri Bienvenu <fikiribienve@gmail.com>
2025-12-05 18:23:20 +0200

    WEB-123: Add Terraform local_file webapp

---
+++ main.tf
@@ -0,0 +1,18 @@
+# Local variable for file content
+locals {
+  app_code = <<EOT
+console.log("Hello from Terraform-created web app!");
+EOT
+}
+
+# Create index.js in a nested folder
+resource "local_file" "webapp_app" {
+  filename = "${path.module}/webapp/src/index.js"  # File path
+  content  = local.app_code                        # File content
+}
+
+# Output the path of the created file
+output "webapp_file_path" {
+  value       = local_file.webapp_app.filename
+  description = "The full path to the web application's index.js file."
+}
